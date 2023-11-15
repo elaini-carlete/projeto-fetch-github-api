f48973e7efc7072d58a7354cdccf9a6d01a88619
Elaini Carlete <elainicarletethomazelli3@gmail.com>
2023-11-15 09:10:25 -0300

    adicionado os fallowers e fallowing

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,8 +1,8 @@
 import { baseUrl } from '../variables.js';
 
 async function getUser(userName){
-    const response = await fetch(`${baseUrl}/${userName}`); 
-    return await response.json(); 
+    const response = await fetch(`${baseUrl}/${userName}`);
+    return await response.json();
 }
 
 export { getUser }
